5e32cb00785ad05b26482030fb2b969d0adb1025
devlyn37 <dev37-2009@hotmail.com>
2019-01-02 13:27:53 -0800

    pull request testing

--- server.js
+++ server.js
@@ -9,5 +9,9 @@ app.get("/", (req, res) => {
   res.status(200).send("Hello World");
 });
 
+app.get("/test", (req, res) => {
+  res.status(200).send("Test");
+});
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
