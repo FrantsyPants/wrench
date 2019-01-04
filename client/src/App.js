c25df83c4f5ca045d8edb5d007b4322a0804b9ef
devlyn37 <dev37-2009@hotmail.com>
2019-01-03 18:01:32 -0800

    Imported ReactStrap, imported bootstrap css, tried some ReactStrap components

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Button } from "reactstrap";
+import { Alert } from "reactstrap";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Alert color="primary">This is a primary alert — check it out!</Alert>
+        <Button color="primary">primary</Button>{" "}
+        <Button color="secondary">secondary</Button>{" "}
+        <Button color="success">success</Button>{" "}
+        <Button color="info">info</Button>{" "}
+        <Button color="warning">warning</Button>{" "}
+        <Button color="danger">danger</Button>{" "}
+        <Button color="link">link</Button>
       </div>
     );
   }
